52de6f96f4312ac3fb85839af5120a5ccc9b3049
elMofaker <155926314+elMofaker@users.noreply.github.com>
2024-06-13 11:16:57 +0300

    g

--- js/home.js
+++ js/home.js
@@ -1,11 +1,30 @@
-const logo = document.querySelectorAll("#logo path");
+// const logo = document.querySelectorAll("#logo path");
 
-for (let i = 0; i < logo.length; i++) {
-    console.log(`Letter ${i} length is ${logo[i].getTotalLength()}`);
-}
+// for (let i = 0; i < logo.length; i++) {
+//     console.log(`Letter ${i} length is ${logo[i].getTotalLength()}`);
+// }
 
 // icon loading
 $(window).on("load", function () {
   $(".content").removeClass("hidden").addClass("fade-in").fadeIn(1000);
   $(".loader").fadeOut(3000, function () {});
+});
+
+// // add and remov classes
+document.querySelector('.btn_login').addEventListener('click', function() {
+  document.querySelector('.hid_form').classList.add('add_top');
+  document.querySelector('.show_form').classList.add('remove_top');
+  setTimeout(function() {
+      document.querySelector('.hid_form').style.display = 'none';
+      document.querySelector('.show_form').style.display = 'block';
+  }, 500);
+});
+
+document.querySelector('.btn_sigini').addEventListener('click', function() {
+  document.querySelector('.hid_form').classList.remove('add_top');
+  document.querySelector('.show_form').classList.remove('remove_top');
+  setTimeout(function() {
+      document.querySelector('.hid_form').style.display = 'block';
+      document.querySelector('.show_form').style.display = 'none';
+  }, 500);
 });
